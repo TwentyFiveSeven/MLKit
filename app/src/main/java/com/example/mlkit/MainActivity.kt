984cfb4e0412268a2af330a9842061a14005b5aa
JinGu <xortm854@naver.com>
2020-05-26 02:04:45 +0900

    Set Permission & Camera action

--- app/src/main/java/com/example/mlkit/MainActivity.kt
+++ app/src/main/java/com/example/mlkit/MainActivity.kt
@@ -1,16 +1,95 @@
 package com.example.mlkit
 
-import android.R.attr.bitmap
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
-
+    val MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1
+    private lateinit var file : File
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val image = FirebaseVisionImage.fromBitmap(bitmap)
+        if (ContextCompat.checkSelfPermission(this@MainActivity,
+                Manifest.permission.READ_CONTACTS)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity,
+                    Manifest.permission.READ_CONTACTS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.READ_CONTACTS),
+                    MY_PERMISSIONS_REQUEST_READ_CONTACTS)
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+        val sdcard : File? = getExternalFilesDir("/sdcard")
+        file = File(sdcard, "capture.pjg")
+        button.setOnClickListener {
+            capture()
+        }
+        //val image = FirebaseVisionImage.fromBitmap(bitmap)
+    }
+
+    private fun capture(){
+        intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(this, "com.bignerdranch.android.test.fileprovider", file))
+        startActivityForResult(intent,101)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 101 && resultCode == Activity.RESULT_OK){
+            val options : BitmapFactory.Options = BitmapFactory.Options()
+            options.inSampleSize = 8;
+            var bitmap : Bitmap = BitmapFactory.decodeFile(file.absolutePath,options)
+            imageView.setImageBitmap(bitmap)
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_READ_CONTACTS -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
     }
 }
